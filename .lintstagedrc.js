deb208fad23947b3f29ad70605bdf9f494f5f01b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-09-02 21:12:46 -0300

    Configure husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Lint & Prettify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
+}
