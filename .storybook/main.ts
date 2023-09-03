2d4899567472305cc1ad3c99234567949a51fb05
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-09-03 17:54:08 -0300

    Add first story and configure preview with GlobalStyles

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 const config = {
   staticDirs: ['../public'],
-  stories: ['..src/components/**/stories.tsx'],
+  stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: StoryObj = {}
