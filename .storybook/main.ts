c31580cb9125bc0c45d223f98cc6f44c12e03021
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-09-03 17:20:38 -0300

    Configure storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+const config = {
+  staticDirs: ['../public'],
+  stories: ['..src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
+}
+export default config
