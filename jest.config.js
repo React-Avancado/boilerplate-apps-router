f1f2c07f44ecfa38197c06d87cf3de3456f88cf4
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-09-03 18:24:16 -0300

    Add plop to generate files

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '!src/app/**', // should be tested in e2e
     '!src/lib/registry.tsx',
     '!src/types/**',
+    '!src/**/stories.tsx',
     '!src/styles/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
